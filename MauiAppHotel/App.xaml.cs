272dad5a7f31e746400b822431d1585ba03d798b
Lucas290919 <luveneroso19@gmail.com>
2025-05-25 22:00:07 -0300

    Resto da Primeira parte

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,8 +1,33 @@
-﻿
+﻿using System.Net.Security;
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quarto = new List<Quarto>()
+        {
+            new Quarto()
+            {
+                descricao = "Suite Super Luxo", ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+            },
+            new Quarto()
+            {
+                descricao = "Suite Luxo", ValorDiariaAdulto = 88.0,
+                ValorDiariaCrianca = 40.0
+            },
+            new Quarto()
+            {
+                descricao = "Suite Single", ValorDiariaAdulto = 50.0,
+                ValorDiariaCrianca = 25.0
+            },
+            new Quarto()
+            {
+                descricao = "Suite Crise", ValorDiariaAdulto = 25.0,
+                ValorDiariaCrianca = 12.5
+            }
+        };
         public App()
         {
             InitializeComponent();
