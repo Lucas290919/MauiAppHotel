902c56f803066ca98056b168ff8f570d1aec2a1a
Lucas290919 <luveneroso19@gmail.com>
2025-06-01 17:40:36 -0300

    Fim do app

---
+++ MauiAppHotel/Models/Hospedagem.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiAppHotel.Models
+{
+    public class Hospedagem
+    {
+        public Quarto Quartoselecioado { get; set; }
+        public int qntAdultos { get; set; }
+        public int qntCrianças { get; set; }
+        public DateTime DataCheckIn { get; set; }
+        public DateTime DataCheckOut { get; set; }
+
+        public int Estadia 
+        { 
+            get => DataCheckOut.Subtract(DataCheckIn).Days;
+        }
+
+        public double ValorTotal
+        {
+            get
+            {
+                double valor_adultos = qntAdultos * Quartoselecioado.ValorDiariaAdulto;
+                double valor_criancas = qntCrianças * Quartoselecioado.ValorDiariaCrianca;
+
+                double total = (valor_adultos +  valor_criancas) * Estadia;
+
+                return total;
+            }
+        }
+    }
+}
--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
@@ -17,14 +19,27 @@ public partial class ContratacaoHospedagem : ContentPage
         dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
     }
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
 		try 
 		{
-			Navigation.PushAsync(new HospedagemContratada()); 
+            Hospedagem h = new Hospedagem
+            {
+                Quartoselecioado = (Quarto)pck_quarto.SelectedItem,
+                qntAdultos = Convert.ToInt32(stp_adultos.Value),
+                qntCrianças = Convert.ToInt32(stp_criancas.Value),
+                DataCheckIn = dtpck_checkin.Date,
+                DataCheckOut = dtpck_checkout.Date
+            };
+
+            await Navigation.PushAsync(new HospedagemContratada()
+            {
+                BindingContext = h
+            }); 
+
 		} catch (Exception ex)
 		{ 
-		DisplayAlert("Ops", ex.Message, "OK");
+		await DisplayAlert("Ops", ex.Message, "OK");
 		}
     }
 
